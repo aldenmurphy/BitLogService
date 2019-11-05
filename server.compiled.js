2246d19474e2ed1f22c897e754e9787b3f4a112f
Patrick Alden Murphy <patmurph@gmail.com>
2019-11-05 13:57:41 -0800

    Remove path dependency

--- server.compiled.js
+++ server.compiled.js
@@ -1,36 +1,34 @@
 // server.js
 //
-import path from 'path';
+// import path from 'path';
 
 const express = require('express');
-
-const {
-  Pool,
-  Client
-} = require('pg');
-
+const { Pool, Client } = require('pg');
 const app = express();
+
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.set('port', process.env.PORT || 4001); //Express only serves static assets in production
+app.set('port', process.env.PORT || 4001);
 
+//Express only serves static assets in production
 console.log('NODE_ENV: ', process.env.NODE_ENV);
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build')); // Return the main index.html, so react-router render the route in the client
+  app.use(express.static('client/build'));
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve('client/build', 'index.html'));
-  });
-} // pools will use environment variables
+  // Return the main index.html, so react-router render the route in the client
+  // app.get('/', (req, res) => {
+  //   res.sendFile(path.resolve('client/build', 'index.html'));
+  // });
+}
+
+// pools will use environment variables
 // for connection information
-
-
 const pool = new Pool({
   user: 'postgres',
   host: 'logbook.cblaufv8epj8.us-east-1.rds.amazonaws.com',
@@ -38,24 +36,36 @@ const pool = new Pool({
   password: 'B!tt3rsw33t2D0',
   port: 5432
 });
+
 const COLUMNS = ['record', 'created'];
+
 app.get('/api/records', (req, res) => {
+
   let queryString = `SELECT id, user_id, record, created from bitlog.log WHERE user_id = '4c867b19-2c1e-4181-848f-4179b66336aa' ORDER BY created DESC`;
+
   pool.query(queryString, function (err, result, fields) {
+
     if (err) throw err;
+
     console.log(result.rows);
     return res.json(result.rows);
   });
 });
+
 app.post('/api/record', (req, res) => {
+
   console.log(req.body);
+
   var user_id = req.body.user_id;
   var record = req.body.record;
+
   var test = 'test';
+
   const query = {
     text: 'INSERT INTO bitlog.log(user_id, record) VALUES($1, $2)',
     values: [user_id, record]
   };
+
   let response = '';
   pool.query(query, (qyueryErr, queryRes) => {
     if (qyueryErr) {
@@ -67,6 +77,7 @@ app.post('/api/record', (req, res) => {
     }
   });
 });
+
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
