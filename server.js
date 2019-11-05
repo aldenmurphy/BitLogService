61b8327c25f3457db397d5315f862f2e6978d543
Patrick Alden Murphy <patmurph@gmail.com>
2019-11-05 14:17:40 -0800

    Remove react client references

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
 // server.js
-//
-// import path from 'path';
 
 const express = require('express');
 const { Pool, Client } = require('pg');
 const app = express();
 
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
 app.set('port', process.env.PORT || 4001);
-
-//Express only serves static assets in production
-console.log('NODE_ENV: ', process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  // Return the main index.html, so react-router render the route in the client
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.resolve('client/build', 'index.html'));
-  // });
-}
 
 // pools will use environment variables
 // for connection information
